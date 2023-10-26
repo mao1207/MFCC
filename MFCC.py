33961a0936a823e1caa7b1a1e11d9674ff9a2f39
Lingjun Mao <78578780+mao1207@users.noreply.github.com>
2023-10-26 23:30:51 +0800

    feat: Update MFCC.py

--- MFCC.py
+++ MFCC.py
@@ -1,8 +1,6 @@
-import numpy
 import scipy.io.wavfile
 from scipy.fftpack import dct
 import os
-from matplotlib import pyplot as plt
 import numpy as np
 import math
 from sklearn.linear_model import LinearRegression
@@ -20,6 +18,7 @@ def compute_delta(vector):
     return delta
 
 class sound_wave():
+    # Constructor: Load the wave file and initialize attributes
     def __init__(self, wave_file):
         script_dir = os.path.dirname(os.path.abspath(__file__))
         self.sample_rate, self.signal = scipy.io.wavfile.read(script_dir + wave_file) 
@@ -41,11 +40,12 @@ class sound_wave():
         self.energy_2 = np.empty(0)
         self.features = np.empty(0)
 
-
+    # Apply pre-emphasis to the signal
     def pre_emphasis(self):
         pre_emphasis = 0.97
         self.emphasized_signal = np.append(self.original_signal[0], self.original_signal[1:] - pre_emphasis * self.original_signal[:-1])
 
+    # Divide the signal into frames
     def frame_division(self):
         frame_size = 0.025
         frame_sample_num = int(frame_size * self.sample_rate)
@@ -57,14 +57,17 @@ class sound_wave():
         for i in range(frame_num):
             self.frame_signal.append(padding_signal[frame_stride_sample_num * i: frame_stride_sample_num * i + frame_sample_num])
 
+    # Apply Hamming window to each frame
     def hamming_window(self):
         for i in range(len(self.frame_signal)):
             self.frame_signal[i] *= np.hamming(len(self.frame_signal[i]))
     
+    # Compute the Short-Time Fourier Transform
     def SIFT(self):
         mag_frames = np.absolute(np.fft.rfft(self.frame_signal, self.NFFT))  # Magnitude of the FFT
         self.pow_frames = (1.0 / self.NFFT) * (mag_frames ** 2)
 
+    # Apply the Mel filter bank on the Fourier transformed signal
     def Mel_Filter_Bank(self):
         mel_points = (700 * (np.exp(np.linspace(self.low_mel, self.high_mel, self.filts_num + 2) / 1127) - 1))
         bin_prequency = np.floor((self.NFFT + 1) * mel_points / self.sample_rate)
@@ -74,19 +77,22 @@ class sound_wave():
             for j in range(int(bin_prequency[i + 1]), int(bin_prequency[i + 2] + 1)):
                 self.H_mel[i][j] = 2 * (bin_prequency[i + 2] - j) / ((bin_prequency[i + 2] - bin_prequency[i]) * (bin_prequency[i + 1] - bin_prequency[i]))
 
+    # Calculate the logarithm of the signal power
     def log_power(self):
         self.filter_frames = np.dot(self.pow_frames, self.H_mel.T)
         self.filter_frames = np.where(self.filter_frames == 0, np.finfo(float).eps, self.filter_frames)
         self.filter_frames = 20 * np.log10(self.filter_frames)
         self.filter_frames -= (np.mean(self.filter_frames, axis=0) + 1e-8)
 
+    # Apply Discrete Cosine Transform to the log powers
     def DCT(self):
         self.mfcc = dct(self.filter_frames, type=2, axis=1, norm='ortho')[:, 1 : 13]
         (frames_num, coeff_num) = self.mfcc.shape
-        lift = 1 + (22 / 2) * np.sin(np.pi * np.arange(coeff_num) / 22)
+        lift = 1 + 11 * np.sin(np.pi * np.arange(coeff_num) / 22)
         self.mfcc *= lift
         self.mfcc -= (np.mean(self.mfcc, axis=0) + 1e-8)
 
+    # Stack features horizontally for the final representation
     def hstack_features(self):
         mfcc_delta_1 = compute_delta(self.mfcc)
         mfcc_delta_2 = compute_delta(mfcc_delta_1)
@@ -96,23 +102,12 @@ class sound_wave():
         energy_delta_2 = compute_delta(energy_delta_1)
         self.features = np.hstack([self.mfcc, energy, mfcc_delta_1, energy_delta_1, mfcc_delta_2, energy_delta_2])
         
+    # Visualize important figures such as spectrograms
     def normalization(self):
         self.features -= np.mean(self.features, axis=0)
         self.features /= np.std(self.features, axis=0)
         
-
-    def print_picture(self):
-        plt.figure(figsize=(11,7), dpi=500)
-        plt.subplot(211)
-        plt.imshow(np.flipud(self.filter_frames.T), cmap=plt.cm.jet, aspect=0.2, extent=[0,self.filter_frames.shape[1],0,self.filter_frames.shape[0]]) #画热力图
-        plt.title("MFCC")
-
-        plt.subplot(212)
-        plt.imshow(np.flipud(self.mfcc.T), cmap=plt.cm.jet, aspect=0.2, extent=[0,self.mfcc.shape[0],0,self.mfcc.shape[1]])#热力图
-        plt.title("MFCC")
-
-        plt.savefig('mfcc_04.png')
-
+    # Combine steps to extract MFCC features
     def get_features(self):
         self.pre_emphasis()
         self.frame_division()
@@ -121,7 +116,6 @@ class sound_wave():
         self.Mel_Filter_Bank()
         self.log_power()
         self.DCT()
-        self.print_picture()
         self.hstack_features()
         self.normalization()
         return self.features
